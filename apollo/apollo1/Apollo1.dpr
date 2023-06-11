559ef48ee05b7be329b2759b8a8b55e042c3903e
iMont-F <gabriel.fz250@outlook.com>
2023-06-11 17:11:06 -0300

    Correções nos arquivos do projeto Apollo1

--- apollo/apollo1/Apollo1.dpr
+++ apollo/apollo1/Apollo1.dpr
@@ -2,13 +2,17 @@ program Apollo1;
 
 uses
   Vcl.Forms,
-  ufApollo1 in 'units\ufApollo1.pas' {Form1};
+  ufApollo1 in 'units\ufApollo1.pas' {Form1},
+  Apollo.Transporte.Product in 'units\Apollo.Transporte.Product.pas',
+  Apollo.Transporte.Creator in 'units\Apollo.Transporte.Creator.pas',
+  Apollo.Transporte.ConcreteProduct in 'units\Apollo.Transporte.ConcreteProduct.pas',
+  Apollo.Transporte.ConcreteCreator in 'units\Apollo.Transporte.ConcreteCreator.pas';
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
-  Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TFApollo1, FApollo1);
   Application.Run;
 end.
