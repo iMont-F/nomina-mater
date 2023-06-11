b677463f8d6713d8efb36ee1df9a4e3bc70a97b6
iMont-F <gabriel.fz250@outlook.com>
2023-06-11 18:48:55 -0300

    APL2-0001 - Estrutura do formulário

--- apollo/apollo2/units/ufApollo2.dfm
+++ apollo/apollo2/units/ufApollo2.dfm
@@ -1,14 +1,111 @@
-object Form2: TForm2
+object FApollo2: TFApollo2
   Left = 0
   Top = 0
-  Caption = 'Form2'
-  ClientHeight = 442
-  ClientWidth = 628
+  Caption = 'Apollo2'
+  ClientHeight = 586
+  ClientWidth = 749
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
   Font.Height = -12
   Font.Name = 'Segoe UI'
   Font.Style = []
+  Position = poScreenCenter
   TextHeight = 15
+  object btnDuvida: TSpeedButton
+    Left = 718
+    Top = 8
+    Width = 23
+    Height = 22
+    DisabledImageName = 'btnDuvida'
+    Caption = '?'
+    OnClick = btnDuvidaClick
+  end
+  object rgConcentrador: TRadioGroup
+    Left = 136
+    Top = 56
+    Width = 473
+    Height = 65
+    Caption = 'Concentrador'
+    Columns = 3
+    ItemIndex = 0
+    Items.Strings = (
+      'CompanyTec'
+      'EzTech'
+      'Gen'#233'rico')
+    TabOrder = 0
+  end
+  object gbLog: TGroupBox
+    Left = 16
+    Top = 248
+    Width = 713
+    Height = 321
+    Caption = 'Log'
+    TabOrder = 1
+    object mmLog: TMemo
+      Left = 2
+      Top = 17
+      Width = 709
+      Height = 302
+      Align = alClient
+      ReadOnly = True
+      TabOrder = 0
+      ExplicitTop = 16
+      ExplicitHeight = 198
+    end
+  end
+  object btnConectar: TButton
+    Left = 48
+    Top = 200
+    Width = 75
+    Height = 25
+    Caption = 'Conectar'
+    TabOrder = 2
+    OnClick = btnConectarClick
+  end
+  object btnStatus: TButton
+    Left = 160
+    Top = 200
+    Width = 75
+    Height = 25
+    Caption = 'Status'
+    TabOrder = 3
+    OnClick = btnStatusClick
+  end
+  object btnEnviar: TButton
+    Left = 272
+    Top = 200
+    Width = 75
+    Height = 25
+    Caption = 'Enviar'
+    TabOrder = 4
+    OnClick = btnEnviarClick
+  end
+  object btnReceber: TButton
+    Left = 384
+    Top = 200
+    Width = 75
+    Height = 25
+    Caption = 'Receber'
+    TabOrder = 5
+    OnClick = btnReceberClick
+  end
+  object btnDesconectar: TButton
+    Left = 496
+    Top = 200
+    Width = 75
+    Height = 25
+    Caption = 'Desconectar'
+    TabOrder = 6
+    OnClick = btnDesconectarClick
+  end
+  object btnLimpar: TButton
+    Left = 608
+    Top = 200
+    Width = 75
+    Height = 25
+    Caption = 'Limpar'
+    TabOrder = 7
+    OnClick = btnLimparClick
+  end
 end
--- apollo/apollo2/units/ufApollo2.pas
+++ apollo/apollo2/units/ufApollo2.pas
@@ -4,21 +4,94 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.Grids, Vcl.StdCtrls,
+  Vcl.Buttons;
 
 type
-  TForm2 = class(TForm)
+  TFApollo2 = class(TForm)
+    rgConcentrador: TRadioGroup;
+    btnDuvida: TSpeedButton;
+    gbLog: TGroupBox;
+    mmLog: TMemo;
+    btnConectar: TButton;
+    btnStatus: TButton;
+    btnEnviar: TButton;
+    btnReceber: TButton;
+    btnDesconectar: TButton;
+    btnLimpar: TButton;
+    procedure btnDuvidaClick(Sender: TObject);
+    procedure btnConectarClick(Sender: TObject);
+    procedure btnStatusClick(Sender: TObject);
+    procedure btnEnviarClick(Sender: TObject);
+    procedure btnReceberClick(Sender: TObject);
+    procedure btnDesconectarClick(Sender: TObject);
+    procedure btnLimparClick(Sender: TObject);
   private
     { Private declarations }
+    procedure UpdateLogMessage;
   public
     { Public declarations }
+    cLogMessage: String;
   end;
 
 var
-  Form2: TForm2;
+  FApollo2: TFApollo2;
 
 implementation
 
 {$R *.dfm}
+
+procedure TFApollo2.btnDuvidaClick(Sender: TObject);
+var
+  cMensagem: String;
+begin
+  cMensagem := 'Esta é uma aplicação que simula diferentes ações que um sistema ' +
+               'pode fazer para diferentes equipamentos Concentradores, isso '     +
+               'utilizando o design pattern Factory Method.';
+  ShowMessage(cMensagem);
+end;
+
+procedure TFApollo2.UpdateLogMessage;
+begin
+  cLogMessage := '['+ DateToStr(Now) +' - ' + TimeToStr(Now) + ']: ';
+end;
+
+procedure TFApollo2.btnConectarClick(Sender: TObject);
+begin
+  UpdateLogMessage;
+  mmLog.Lines.Add(cLogMessage + 'Funcionalidade "Conectar" em processo de implementação');
+end;
+
+procedure TFApollo2.btnStatusClick(Sender: TObject);
+begin
+  UpdateLogMessage;
+  mmLog.Lines.Add(cLogMessage + 'Funcionalidade "Status" em processo de implementação');
+end;
+
+procedure TFApollo2.btnEnviarClick(Sender: TObject);
+begin
+  UpdateLogMessage;
+  mmLog.Lines.Add(cLogMessage + 'Funcionalidade "Enviar" em processo de implementação');
+end;
+
+
+procedure TFApollo2.btnReceberClick(Sender: TObject);
+begin
+  UpdateLogMessage;
+  mmLog.Lines.Add(cLogMessage + 'Funcionalidade "Receber" em processo de implementação');
+end;
+
+procedure TFApollo2.btnDesconectarClick(Sender: TObject);
+begin
+  UpdateLogMessage;
+  mmLog.Lines.Add(cLogMessage + 'Funcionalidade "Desconectar" em processo de implementação');
+end;
+
+procedure TFApollo2.btnLimparClick(Sender: TObject);
+begin
+  UpdateLogMessage;
+  mmLog.Lines.Clear;
+  mmLog.Lines.Add(cLogMessage + 'Log limpo com sucesso!');
+end;
 
 end.
